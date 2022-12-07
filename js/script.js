1c37482c1666d1065af60bb26d7bbf7a1bbc0580
Stormfall13 <85984394+Stormfall13@users.noreply.github.com>
2022-12-08 01:10:07 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,3 +29,65 @@ autoplay: {
     delay: 3000,
 }
 });
+
+
+
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
+
+
+
+
+
+const input = document.querySelectorAll('.input');
+const btnForm = document.querySelector('.btn__valid');
+const validText = document.querySelector('.valid');
+const titleForm = document.querySelector('.container__form > .title')
+const secondTitle = document.querySelector('.second__title')
+
+
+btnForm.addEventListener("click", function(){
+    for (inputs of input ){
+        inputs.style.display = 'none';
+    }
+    btnForm.style.display = 'none';
+    validText.style.display = 'block';
+    titleForm.style.display = 'none';
+    secondTitle.style.display = 'block';
+});
+
+
+const popup = document.querySelector('#popup__form')
+const btnOpen = document.querySelectorAll('.btn')
+const closePopup = document.querySelector('.close__popup')
+
+for (let btnOp of btnOpen){
+    btnOp.addEventListener("click", function(){
+        popup.style.display = 'flex';
+        function popupOpacity(){
+            popup.style.opacity = '1';
+        }
+        setTimeout(popupOpacity, 700);
+    })
+}
+
+
+
+closePopup.addEventListener("click", function(){
+    popup.style.opacity = '0';
+    function popupNone(){
+        popup.style.display = 'none';
+    }
+    setTimeout(popupNone, 700);
+})
